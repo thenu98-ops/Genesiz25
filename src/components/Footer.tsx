a26c6f2307b019d827a6ba3e3be1f140921a87af
thenura <thenura125@gmail.com>
2025-07-16 00:24:03 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: React.FC = () => {
               },
               {
                 icon: <LinkedinIcon className="w-5 h-5" />,
-                link: "#",
+                link: "https://www.linkedin.com/company/eric-kdu/",
               },
             ].map((social, index) => (
               <a
